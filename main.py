a10f43518f76d06a04ff2fd7a92284f0c611e5ad
Frederique Poitras <coding.cosmo.3301@proton.me>
2025-08-28 19:41:31 -0500

    Add Python and PDF files

---
+++ main.py
@@ -0,0 +1,10 @@
+import rooms
+
+print("Welcome to my test run to github. Here, I will be developing a simple game that I will be uploading to github. " \
+"Throughout this project, I will be practice push, pull, and etc. steps. ")
+input("Press any key to enter the maze. [ENTER]")
+
+RUNNING = True
+while RUNNING: 
+    print(OPTIONS_A)
+
---
+++ rooms.py
@@ -0,0 +1,22 @@
+OPTIONS_A = """
+Options: 
+        (a) View KB
+        (b) Right
+        (c) Left
+        (d) Forward"""
+
+OPTIONS_B = """
+Options: 
+        (a) View KB
+        (b) Right
+        (c) Left
+        (d) Forward
+        (e) Below"""
+
+OPTIONS_C = """
+Options: 
+        (a) View KB
+        (b) Right
+        (c) Left
+        (d) Forward
+        (e) Below"""
